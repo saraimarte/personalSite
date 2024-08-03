b544b6ed4967654e16b96be0192236bb24ea894e
Sarai Marte <smartepujols@gmail.com>
2024-08-02 21:32:57 -0400

    deployment

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,19 +11,6 @@ const blogCollection = defineCollection({
 });
 
 
-const bibleCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    author: z.string(),
-    tags: z.array(z.string()),
-    phase: z.string()
-  }),
-});
-
-
 export const collections = {
-  blog: blogCollection,
-  bible: bibleCollection
+  blog: blogCollection
 };
