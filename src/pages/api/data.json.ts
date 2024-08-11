8b71c895965f2dc51a2ed7e097e04f16708a63f9
Sarai Marte <smartepujols@gmail.com>
2024-08-10 20:10:01 -0400

    081024 made code blocks mobile friendly by changing white-space from pre to pre-wrap + added drop down component + changed some styles for h1 and h2 title and dates in layouts and slug page + finished post 4 + edited post 1 + sorted posts by most recent both in the index and data.json

--- src/pages/api/data.json.ts
+++ src/pages/api/data.json.ts
@@ -3,10 +3,16 @@ import { getCollection } from 'astro:content';
 import type { CollectionEntry } from 'astro:content';
 
 export const GET: APIRoute = async (): Promise<Response> => {
+  // Fetch all blog posts
   const allBlogArticles: CollectionEntry<'blog'>[] = await getCollection('blog');
 
-  // Map the collection to include both data and slug
-  const blogData = allBlogArticles.map(article => ({
+  // Sort blog posts by pubDate in descending order
+  const sortedBlogArticles = allBlogArticles.sort((a, b) => {
+    return new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime();
+  });
+
+  // Map the sorted collection to include both data and slug
+  const blogData = sortedBlogArticles.map(article => ({
     ...article.data, // Spread the post data
     slug: article.slug // Add the slug
   }));
